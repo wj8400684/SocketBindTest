637cc069b3e56bf048577771201e1e9744cc936d
wujun <8400684@qq.com>
2024-12-11 23:44:36 +0800

    ini

---
+++ SocketBindTest.Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using SuperSocket.Client;
+using SuperSocket.Connection;
+
+var remo = new IPEndPoint(IPAddress.Parse("192.168.124.53"), 8080);
+
+var tasks = Enumerable
+    .Range(0, 1)
+    .Select(_ => RunConnectionAsync())
+    .ToArray();
+
+var rounds = await Task.WhenAll(tasks);
+
+Console.WriteLine($"Rounds:{rounds.Length}");
+
+Console.ReadKey();
+
+async Task<int> RunConnectionAsync()
+{
+    IConnector connector = new SocketConnector();
+
+    var state = await connector.ConnectAsync(remo);
+
+    var connection = state.CreateConnection(new ConnectionOptions());
+
+    for (int i = 0; i < 10; i++)
+    {
+        await connection.SendAsync(Encoding.UTF8.GetBytes("Hello World"));
+
+        await Task.Delay(1000);
+    }
+    
+    return 0;
+}
--- SocketBindTest.Server/TelenetConnectionHandler.cs
+++ SocketBindTest.Server/TelenetConnectionHandler.cs
@@ -1,23 +1,54 @@
 using System.IO.Pipelines;
 using System.Net;
+using System.Net.Security;
 using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Connections;
 
 namespace SocketBindTest.Server;
 
-internal sealed class TelnetConnectionHandler(ILogger<TelnetConnectionHandler> logger) : ConnectionHandler
+internal sealed class TelnetConnectionHandler : ConnectionHandler
 {
-    private readonly DnsEndPoint _remoteEndPoint = new("www.ip138.com", 443, AddressFamily.InterNetwork);
+    private readonly List<IPAddress> _ipAddresses;
+    private readonly ILogger<TelnetConnectionHandler> _logger;
+    private readonly DnsEndPoint _remoteEndPoint = new("www.bejson.com", 443, AddressFamily.InterNetwork);
+
+    public TelnetConnectionHandler(ILogger<TelnetConnectionHandler> logger)
+    {
+        _logger = logger;
+        _ipAddresses = Dns.GetHostAddresses(Dns.GetHostName())
+            .Where(address => address.AddressFamily == AddressFamily.InterNetwork).ToList();
+
+        foreach (var address in _ipAddresses)
+        {
+            _logger.LogInformation($"本地ip{address}");
+        }
+    }
+
+    private IPAddress? GetLocalIpAddress(IPAddress address)
+    {
+        return _ipAddresses.FirstOrDefault(s => s.Equals(address));
+    }
 
     public override async Task OnConnectedAsync(ConnectionContext connection)
     {
+        var port = connection.RemoteEndPoint switch
+        {
+            IPEndPoint ip => ip.Port,
+            DnsEndPoint dns => dns.Port,
+            _ => throw new NotImplementedException(),
+        };
+
         if (connection.LocalEndPoint is not IPEndPoint localEndPoint)
         {
-            logger.LogWarning("ip不正确");
+            _logger.LogWarning("ip不正确");
             return;
         }
 
-        logger.LogInformation("新连接，远程地址-{RemoteEndPoint}-{LocalEndPoint}", connection.RemoteEndPoint,
+        var address = localEndPoint.Address.MapToIPv4();
+        
+        _logger.LogInformation("新连接，远程地址-{RemoteEndPoint}-{LocalEndPoint}", connection.RemoteEndPoint,
             connection.LocalEndPoint);
 
         var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
@@ -25,32 +56,60 @@ internal sealed class TelnetConnectionHandler(ILogger<TelnetConnectionHandler> l
         try
         {
             socket.ExclusiveAddressUse = false;
-            socket.Bind(localEndPoint);
+            socket.Bind(new IPEndPoint(address, port));
         }
         catch (Exception e)
         {
-            logger.LogError(e, "绑定ip失败，远程地址-{RemoteEndPoint}-{LocalEndPoint}", connection.RemoteEndPoint,
+            _logger.LogError(e, "绑定ip失败，远程地址-{RemoteEndPoint}-{LocalEndPoint}", connection.RemoteEndPoint,
                 connection.LocalEndPoint);
             return;
         }
 
-        await socket.ConnectAsync(_remoteEndPoint);
-
-        var stream = new NetworkStream(socket, true);
+        using var timeout = new CancellationTokenSource(TimeSpan.FromSeconds(15));
+        using var cancellationTokenSource =
+            CancellationTokenSource.CreateLinkedTokenSource(connection.ConnectionClosed, timeout.Token);
 
         try
         {
-            await connection.Transport.Input.CopyToAsync(stream);
-            await stream.CopyToAsync(connection.Transport.Output);
+            await socket.ConnectAsync(_remoteEndPoint, cancellationTokenSource.Token);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "连接远程服务器失败，远程地址-{RemoteEndPoint}-{LocalEndPoint}", connection.RemoteEndPoint,
+                connection.LocalEndPoint);
+            return;
+        }
+
+        Stream stream = new NetworkStream(socket, true);
+
+        _logger.LogInformation("开始传输数据-{RemoteEndPoint}-{LocalEndPoint}", connection.RemoteEndPoint,
+            connection.LocalEndPoint);
+
+        try
+        {
+            await connection.Transport.Input.CopyToAsync(stream, CancellationToken.None);
+            await stream.CopyToAsync(connection.Transport.Output, CancellationToken.None);
         }
         catch (Exception e)
         {
-            logger.LogError(e, "ip不正确");
+            _logger.LogError(e, "ip不正确");
         }
         finally
         {
-            logger.LogWarning("断开连接啦，远程地址-{RemoteEndPoint}-{LocalEndPoint}", connection.RemoteEndPoint,
+            _logger.LogWarning("断开连接啦，远程地址-{RemoteEndPoint}-{LocalEndPoint}", connection.RemoteEndPoint,
                 connection.LocalEndPoint);
         }
+    }
+
+    private bool OnRemoteCertificateValidationCallback(object sender, X509Certificate? certificate, X509Chain? chain,
+        SslPolicyErrors sslPolicyErrors)
+    {
+        return true;
+    }
+
+    private X509Certificate OnLocalCertificateSelectionCallback(object sender, string targetHost,
+        X509CertificateCollection localCertificates, X509Certificate? remoteCertificate, string[] acceptableIssuers)
+    {
+        return null;
     }
 }
