3e657bc8ba5c476019d10f5d0c305ae780819af2
wujun <8400684@qq.com>
2024-12-18 17:35:56 +0800

    ini

---
+++ SocketBindTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var ipAddress = Dns.GetHostAddresses(Dns.GetHostName()).Where(ip => !ip.Equals(IPAddress.Loopback)).ToArray();
+
+foreach (var ip in ipAddress)
+{
+    Console.WriteLine("本地ip:{0}", ip.ToString());
+}
+
+var bindIp = new IPEndPoint(ipAddress.First(), Random.Shared.Next(10000, 65535));
+
+using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+socket.ExclusiveAddressUse = false;
+socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+socket.Bind(bindIp);
+
+Console.WriteLine("正在连接");
+
+await socket.ConnectAsync(new DnsEndPoint("www.baidu.com", 80, AddressFamily.InterNetwork));
+
+Console.WriteLine("连接成功");
+
+Console.ReadKey();
